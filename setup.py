37d708cdddcd929ccd1b9d7cf8cd55015b41ec28
Wesley Hsieh <iamwesleyhsieh@gmail.com>
2016-01-19 00:22:06 -0800

    Initial Setup for PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-m distutils.core import setup
+from distutils.core import setup
 setup(
   name = 'policy_gradient',
   packages = ['policy_gradient'], # this must be the same as the name above
@@ -6,8 +6,8 @@ setup(
   description = 'Policy Gradient Package',
   author = 'Wesley Hsieh',
   author_email = 'wesley.y.hsieh@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/WesleyHsieh/policy_gradient.git', # use the URL to the github repo
+  download_url = 'https://github.com/wesleyhsieh/policy_gradient/tarball/0.1', # I'll explain this in a second
+  keywords = [], # arbitrary keywords
   classifiers = [],
 )
